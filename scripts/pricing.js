e6e5200dc5b2bb7663e00110061beb06320450d2
CJLynch01 <cjlynch@byui.edu>
2023-12-21 20:05:24 -0700

    Save

--- scripts/pricing.js
+++ scripts/pricing.js
@@ -4,23 +4,23 @@ const cards = document.querySelector('#cards');
 async function getPricingData() {
     const response = await fetch(urlgit);
     const data = await response.json();
-    displayPricing(data.pricing);
+    displayPricing(data.shirts);
 }
 
 getPricingData();
 
-const displayPricing = (pricing) => {
-    pricing.forEach((cost) => {
+const displayPricing = (shirts) => {
+    shirts.forEach((product) => {
         let card = document.createElement('section');
         let name = document.createElement('h3');
         let price = document.createElement('h4');
         let image = document.createElement('img');
   
-        name.textContent = `${cost.name}`;
-        price.textContent = `Walk-In - Half Day: ${cost.price}`;
+        name.textContent = `${product.name}`;
+        price.textContent = `Walk-In - Half Day: ${product.price}`;
 
-        image.setAttribute('src', cost.image);
-        image.setAttribute('alt', `Vehicle Type ${cost.name}`);
+        image.setAttribute('src', product.image);
+        image.setAttribute('alt', `Vehicle Type ${product.name}`);
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '340');
         image.setAttribute('height', '440');
